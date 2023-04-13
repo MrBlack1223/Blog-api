3269087c0e6a3d196963d0ec2ce7b03452cfb20d
Wiktor Tarnowski <MrBlack1223@wp.pl>
2023-04-13 18:25:43 +0200

    changing cookie options

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -52,7 +52,7 @@ module.exports = {
         .json(data)
     },
     logout : async(req,res,next) => {
-        res.clearCookie('acces_token',{domain: 'random-blogs-api.onrender.com',path: '/'})
+        res.clearCookie('acces_token',{domain: 'random-blogs-api.onrender.com', path: '/',secure: true, httpOnly: true, sameSite: 'none'})
         res.send("CookieCleared")
     }
 }
